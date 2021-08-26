54678db86b1ad338163ffe28f362bebf354b3ed6
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-08-26 15:47:14 +0530

    Sign up page is created

---
+++ FrontEndApplication/smart_locker/lib/widgets/submitbutton.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SubmitButton extends StatelessWidget {
+  final VoidCallback onSubmitHandler;
+  final String text;
+
+  SubmitButton({required this.onSubmitHandler, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 42.5,
+      width: 135.0,
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          colors: [
+            Colors.indigo.shade900,
+            Colors.lightBlue.shade900,
+            Colors.blue.shade800,
+          ],
+          begin: FractionalOffset.centerLeft,
+          end: FractionalOffset.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: TextButton(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 17.5,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        onPressed: onSubmitHandler,
+      ),
+    );
+  }
+}
