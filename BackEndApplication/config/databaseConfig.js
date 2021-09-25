42c15d649e9ebadf5e5f92fbb9310e4273f02f12
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-09-25 15:55:44 +0530

    developing user authentication and authorization

--- BackEndApplication/config/databaseConfig.js
+++ BackEndApplication/config/databaseConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 config = {
    host: 'localhost',
    user: 'root',
-   password: '', // password
+   password: 'maths123456', // password
    database: 'SMARTLOCKERSYSTEM' // databasename
 }
 
--- BackEndApplication/routes/login.js
+++ BackEndApplication/routes/login.js
@@ -1,1 +1,38 @@
 // set database password before running the app
+const config = require('../config/databaseConfig');
+const express = require('express');
+const Joi = require('joi');
+const bcrypt = require('bcrypt'); 
+
+const router = express.Router();
+
+const connection= config.connection;
+
+router.post('/', (req, res) => {
+
+    //validating request body
+    const schema = Joi.object({
+        email: Joi.string().min(3).max(100).required().email(),
+        password: Joi.string().min(8).max(25).required()
+    });
+
+    const result = schema.validate(req.body);
+
+    if(result.error){
+        return res.status(400).send(result.error.details[0].message);
+    }
+
+    // retrive from database
+    connection.query('SELECT * FROM user WHERE UserEmail = ?',[req.body.email], (err, rows, fields) => {
+        if(err) return res.send("Database failure");
+        if(!rows.length ) return res.status(400).send("Invalid email address");
+        if(rows.length){
+            bcrypt.compare(req.body.password, rows[0].Password, (errHash, resultHash) => {
+               if(!resultHash) return res.status(400).send("Incorrect Password");
+               res.send(true);
+            });
+        }
+    });
+});
+
+module.exports = router;
