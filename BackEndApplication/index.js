8c909aaf9877d4ba0b44e9c99abc9becbf7c7d99
Sandun666 <spriyashan666@gmail.com>
2021-09-25 20:02:33 +0530

    some backend

--- BackEndApplication/index.js
+++ BackEndApplication/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const home = require('./routes/home');
 const users = require('./routes/users');
+
+// create express app
 const app = express();
 
 app.use(express.json()); // convert to json
 app.use('/', home);
 app.use('/api/users', users);
 
-// listening port 3000
+// setup the server port
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log(`Listening port ${port}...`));
 
+// listening port 3000
+app.listen(port, () => {
+    console.log(`Listening port ${port}...`)
+});
--- BackEndApplication/routes/users.js
+++ BackEndApplication/routes/users.js
@@ -3,16 +3,15 @@ const config = require('../config/databaseConfig');
 
 const router = express.Router();
 
-const connection= config.connection;
+const connection = config.connection;
 
 router.get('/', (req, res) => {
     connection.query('SELECT * FROM user', (err, rows, fields) => {
-        if(err){
-           return res.send("Database failure");
+        if (err) {
+            return res.send("Database failure");
         }
         res.send(rows);
     });
 });
 
 module.exports = router;
-
