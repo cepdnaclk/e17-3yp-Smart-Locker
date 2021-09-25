c6076fa63d4de11c1de94fc3855df0cd0168ed7a
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-09-25 11:08:18 +0530

    developing user authentication and authorization

--- BackEndApplication/routes/users.js
+++ BackEndApplication/routes/users.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const Joi = require('joi');
+const { v4: uuidv4 } = require('uuid');
 const config = require('../config/databaseConfig');
 
 const router = express.Router();
 
 const connection= config.connection;
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
 
+    //validating request body
     const schema = Joi.object({
         username: Joi.string().min(8).max(60).required(),
         email: Joi.string().min(3).max(100).required().email(),
@@ -21,11 +23,21 @@ router.get('/', (req, res) => {
         return res.status(400).send(result.error.details[0].message);
     }
 
-    connection.query('SELECT * FROM user', (err, rows, fields) => {
-        if(err){
-           return res.send("Database failure");
+    // prevent from injection attacks
+    connection.query('SELECT * FROM user WHERE UserEmail = ?',[req.body.email], (err, rows, fields) => {
+        if(err) return res.send("Database failure");
+        if(rows.length ) return res.status(400).send("User already exist");
+        if(!rows.length){
+            connection.query('INSERT INTO user(UserName, UserEmail, UserId, Password, MobileNumber) values (?, ?, ?, ?, ?)',
+            [req.body.username,
+            req.body.email,
+            uuidv4(),
+            req.body.password,
+            req.body.mobile], (errInsert, resulInsert) => {
+                if(errInsert) return res.send("Database failure");
+                res.send("Registration succeeded");
+            });
         }
-        res.send(rows);
     });
 });
 
