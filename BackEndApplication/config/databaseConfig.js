e3a4fe244af21605e533a8d66325d3f1b30699fe
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-09-25 14:32:18 +0530

    developing user authentication and authorization

--- BackEndApplication/config/databaseConfig.js
+++ BackEndApplication/config/databaseConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 config = {
    host: 'localhost',
    user: 'root',
-   password: 'maths123456', // password
+   password: '', // password
    database: 'SMARTLOCKERSYSTEM' // databasename
 }
 
