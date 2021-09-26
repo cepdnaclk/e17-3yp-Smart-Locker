c0c28d1fdf207fa3fddcf9f3711074ca66bdeb92
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-09-26 10:05:33 +0530

    configuring jwt

--- BackEndApplication/routes/login.js
+++ BackEndApplication/routes/login.js
@@ -2,8 +2,8 @@
 const config = require('../config/databaseConfig');
 const express = require('express');
 const Joi = require('joi');
-
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -27,13 +27,13 @@ router.post('/', (req, res) => {
 
     // retrive from database
     connection.query('SELECT * FROM user WHERE UserEmail = ?',[req.body.email], (err, rows, fields) => {
-        if(err) return res.send("Database failure");
+        if(err) return res.status(500).send("Database failure");
         if(!rows.length ) return res.status(400).send("Invalid email address");
         if(rows.length){
             bcrypt.compare(req.body.password, rows[0].Password, (errHash, resultHash) => {
                if(!resultHash) return res.status(400).send("Incorrect Password");
-               res.send(true);
-
+               const token = jwt.sign({jwtEmail: rows[0].UserEmail}, 'smartLocker_jwtPrivateKey');
+               res.header('x-auth-token', token).send('Successfully logged');
             });
         }
     });
