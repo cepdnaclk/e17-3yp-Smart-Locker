8608e576067cbe8b6484938c2b5f28c5d7f40ed0
Sandun666 <spriyashan666@gmail.com>
2021-10-01 15:51:57 +0530

    tokengeneratefunctionedit

--- BackEndApplication/routes/purchase.js
+++ BackEndApplication/routes/purchase.js
@@ -8,7 +8,7 @@ const connection = config.connection;
 
 function generateToken(length) {
     var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters = 'ABCD0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
