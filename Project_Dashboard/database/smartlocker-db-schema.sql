ea51ec461ad1d200656a42a883320867979c7ae0
Dadoright <donabusi1@gmail.com>
2021-10-20 02:40:51 +0530

    Dashboard with routes

---
+++ Project_Dashboard/database/smartlocker-db-schema.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE SMARTLOCKERSYSTEM;
+USE SMARTLOCKERSYSTEM;
+
+CREATE TABLE Role(
+    RoleID char(60) not null PRIMARY KEY,
+    RoleName char(60)
+);
+
+CREATE TABLE User(
+	UserName char(60),
+	UserEmail char(100),
+    UserID char(60) not null,
+    Password char(255),
+    MobileNumber char(20),
+    UserRoleID char(60),
+
+    PRIMARY KEY(UserID), 
+    FOREIGN KEY(UserRoleID) REFERENCES Role(RoleID) ON DELETE CASCADE 
+);
+
+CREATE TABLE Location(
+	LocationID char(60),
+    Longitude char(60),
+    Latitude char(60),
+    LocationDescription char(60),
+    LocationUrl char(250),
+	PRIMARY KEY(LocationID)
+);
+
+CREATE TABLE Locker(
+	LockerID char(60),
+    LockerUserID char(60),
+    LockerLocationID char(60),
+    ClusterID char(60),
+    Availability boolean,
+    IsEmpty boolean,
+    IsLocked boolean,
+    OneTimeToken char(60),
+    SharedOneTimeToken char(60),
+    LockerNumber int,
+    ExpireDate DATETIME,
+    
+    PRIMARY KEY(LockerID),
+    FOREIGN KEY(LockerUserID) REFERENCES User(UserID),
+    FOREIGN KEY(LockerLocationID) REFERENCES Location(LocationID)
+);
---
+++ Project_Dashboard/server/routes/addlocker.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const config = require('../config/databaseConfig');
+
+const router = express.Router();
+const connection = config.connection;
+
+router.post('/', (req, res) => {
+  const lockerid = req.body.lockerid;
+  const lockerlocationid = req.body.lockerlocationid;
+  const lockernumber = req.body.lockernumber;
+
+  connection.query(
+    'INSERT INTO Locker(LockerID,LockerLocationID,LockerNumber) values (?,?,?)',
+    [lockerid, lockerlocationid, lockernumber],
+    (errInsert) => {
+      if (errInsert) return res.status(500).send('Database Failure');
+      res.send('Entry Successful');
+    }
+  );
+});
+
+module.exports = router;
