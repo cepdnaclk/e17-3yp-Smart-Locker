550d7c7069665560c8ffbe5ffc5c25b1bd70cc34
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-10-20 22:32:09 +0530

    backend updated

--- BackEndApplication/routes/home.js
+++
@@ -1,9 +0,0 @@
-// set database password before running the app
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("Home");
-});
-
-module.exports = router;
