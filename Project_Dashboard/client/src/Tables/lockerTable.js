fa1627d3b392f5167dbfc19e47475867b7925c43
Dadoright <donabusi1@gmail.com>
2021-10-20 12:50:24 +0530

    Tables Finished

--- Project_Dashboard/client/src/Tables/lockerTable.js
+++ Project_Dashboard/client/src/Tables/lockerTable.js
@@ -2,51 +2,48 @@ import Axios from 'axios';
 import { useState } from 'react';
 
 function LockerTable() {
-  const [lockerList, setLockerList] = useState('');
+  const [lockerList, setLockerList] = useState([]);
 
   const getLocker = () => {
     Axios.get('http://localhost:3001/sendlocker').then((response) => {
-      console.log(response);
+      //console.log(response);
       setLockerList(response.data);
     });
   };
+  getLocker();
+  const renderlockerlist = (val, key) => {
+    return (
+      <tr key={key}>
+        <td>{key}</td>
+        <td>{val.LockerID}</td>
+        <td>{val.LockerUserID}</td>
+        <td>{val.LockerLocationID}</td>
+        <td>{val.Availability}</td>
+        <td>{val.ExpireDate}</td>
+        <td>{val.IsEmpty}</td>
+        <td>{val.LockerNumber}</td>
+      </tr>
+    );
+  };
   return (
     <div>
-      <button className="btn btn-secondary" onClick={getLocker}>
-        Load Locker
-      </button>
-      {lockerList.map((val, key) => {
-        return (
-          <div>
-            <table className="table table-bordered mx-2 my-3">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Locker ID</th>
-                  <th scope="col">Locker User ID</th>
-                  <th scope="col">Locker Location ID</th>
-                  <th scope="col">Availability</th>
-                  <th scope="col">ExpireDate</th>
-                  <th scope="col">IsEmpty</th>
-                  <th scope="col">Locker Number</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>{val.LockerID}</td>
-                  <td>{val.LockerUserID}</td>
-                  <td>{val.LockerLocationID}</td>
-                  <td>{val.Availability}</td>
-                  <td>{val.ExpireDate}</td>
-                  <td>{val.IsEmpty}</td>
-                  <td>{val.LockerNumber}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        );
-      })}
+      <div>
+        <table className="table table-bordered mx-2 my-3">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Locker ID</th>
+              <th scope="col">Locker User ID</th>
+              <th scope="col">Locker Location ID</th>
+              <th scope="col">Availability</th>
+              <th scope="col">ExpireDate</th>
+              <th scope="col">IsEmpty</th>
+              <th scope="col">Locker Number</th>
+            </tr>
+          </thead>
+          <tbody>{lockerList.map(renderlockerlist)}</tbody>
+        </table>
+      </div>
     </div>
   );
 }
