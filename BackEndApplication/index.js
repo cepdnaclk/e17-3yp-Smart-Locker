5e729233f0c048e9807eba88b4fe25e8057bf9f7
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-10-20 23:13:26 +0530

    index.js updated

--- BackEndApplication/index.js
+++ BackEndApplication/index.js
@@ -1,18 +1,14 @@
 // set database password before running the app
 const express = require('express');
-const home = require('./routes/home');
 const users = require('./routes/users');
 const mapclick = require('./routes/mapclick');
 const controlPanel = require('./routes/controlPanel');
-const accountProfile = require('./routes/accountProfile');
 const purchase = require('./routes/purchase');
-const lockerdetails = require('./routes/lockerdetails')
-    // create express app
+const lockerdetails = require('./routes/lockerdetails');
 const login = require('./routes/login');
 const app = express();
 
 app.use(express.json()); // convert to json
-app.use('/', home);
 app.use('/api/users', users);
 app.use('/api/login', login);
 app.use('/api/mapclick', mapclick);
