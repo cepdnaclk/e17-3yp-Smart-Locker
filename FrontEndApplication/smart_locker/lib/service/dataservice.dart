01005ada4eabda1cc0958d87b4c7db60b218b124
Sandun666 <spriyashan666@gmail.com>
2021-10-22 11:58:12 +0530

    set

--- FrontEndApplication/smart_locker/lib/service/dataservice.dart
+++ FrontEndApplication/smart_locker/lib/service/dataservice.dart
@@ -2,14 +2,15 @@ import 'package:flutter/foundation.dart';
 
 import 'package:flutter/material.dart';
 import 'package:smart_locker/models/LocationsModel.dart';
+import 'package:smart_locker/models/LockerLocationDetailsModel.dart';
 import 'package:smart_locker/models/PurchasedLockersModel.dart';
 import 'package:smart_locker/models/UserModel.dart';
 
 class DataService extends ChangeNotifier {
   static UserModel user = UserModel();
   static List<LocationsModel> location = [];
-  static List<LocationsModel> mylocations = [];
   static List<PurchasedLockersModel> purchedLockers = [];
+  static List<LockerLocationDetailsModel> userLockers = [];
   static List<PurchasedLockersModel> avilableLockers = [];
   static String ip = "http://34.227.193.145:3000";
   static String jwt = "";
