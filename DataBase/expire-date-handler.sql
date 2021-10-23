9cd978e3022b3fe88529c0ad713bf8012704ce58
AdithyaGallage <e17091@eng.pdn.ac.lk>
2021-10-23 11:56:44 +0530

    backend and database updated. New Login table is added & direct purchase endpoint added

--- DataBase/expire-date-handler.sql
+++ DataBase/expire-date-handler.sql
@@ -14,5 +14,21 @@ DELIMITER ;
 DROP EVENT IF EXISTS setAvailability;
 CREATE EVENT setAvailability
     ON SCHEDULE EVERY 1 SECOND
+    STARTS NOW()
     DO
       CALL checking_expire();
+
+DROP PROCEDURE IF EXISTS erase_loginrecords;
+DELIMITER $$
+CREATE PROCEDURE erase_loginrecords()
+BEGIN
+	DELETE FROM Login;
+END$$
+DELIMITER ;
+
+DROP EVENT IF EXISTS erase_event;
+CREATE EVENT erase_event
+    ON SCHEDULE EVERY 24 HOUR
+    STARTS NOW()
+    DO
+      CALL erase_loginrecords;
