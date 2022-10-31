9a1dfe9d142288b1ae08ffe0fcc16fe84435d1ec
AdithyaGallage <e17091@eng.pdn.ac.lk>
2022-10-31 22:10:32 +0530

    some updates done

--- BackEndApplication/config/databaseConfig.js
+++ BackEndApplication/config/databaseConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 config = {
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: 'maths123456',
   database: 'SMARTLOCKERSYSTEM', // databasename
 };
 
